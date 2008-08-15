9381dd19d48d4c1b336f56a171c2b422e7e7c2a0
shinysky1986 <shinysky1986@dc6a4a2f-8f50-0410-ba58-3d5ebe76e9aa>
2008-08-15 11:30:53 +0000

    switch to httpclient-4.0-alpha5-SNAPSHOT.jar(from httpclient3.1)

--- src/facebookchat/common/ErrorCode.java
+++ src/facebookchat/common/ErrorCode.java
@@ -1,21 +1,57 @@
 package facebookchat.common;
 
-public class ErrorCode {
-	public static Long kError_Global_ValidationError = 1346001L;
-	public static Long kError_Login_GenericError = 1348009L;
-	public static Long kError_Chat_NotAvailable = 1356002L;
-	public static Long kError_Chat_SendOtherNotAvailable = 1356003L;
-	public static Long kError_Async_NotLoggedIn = 1357001L;
-	public static Long kError_Async_LoginChanged = 1357003L;
-	public static Long kError_Async_CSRFCheckFailed = 1357004L;
-	public static Long kError_Chat_TooManyMessages = 1356008L;
-	public static Long kError_Platform_CallbackValidationFailure = 1349007L;
-	public static Long kError_Platform_ApplicationResponseInvalid = 1349008L;
-	
-	public static Long Error_Global_NoError = 0L;
-	public static Long Error_Async_HttpConnectionFailed = 1001L;
-	public static Long Error_Async_UnexpectedNullResponse = 1002L;
-	public static Long Error_System_UIDNotFound = 1003L;
-	public static Long Error_System_ChannelNotFound = 1004L;
-	public static Long Error_System_PostFornIDNotFound = 1005L;
+/*
+ * SIP Communicator, the OpenSource Java VoIP and Instant Messaging client.
+ * 
+ * Distributable under LGPL license. See terms of license at gnu.org.
+ */
+
+/**
+ * Facebook Error Code
+ * 
+ * @author Dai Zhiwei
+ * 
+ */
+public class ErrorCode
+{
+    public static int kError_Global_ValidationError = 1346001;
+
+    public static int kError_Login_GenericError = 1348009;
+
+    public static int kError_Platform_CallbackValidationFailure = 1349007;
+
+    public static int kError_Platform_ApplicationResponseInvalid = 1349008;
+
+    public static int kError_Chat_NotAvailable = 1356002;
+
+    public static int kError_Chat_SendOtherNotAvailable = 1356003;
+
+    public static int kError_Chat_TooManyMessages = 1356008;
+
+    public static int kError_Async_NotLoggedIn = 1357001;
+
+    public static int kError_Async_LoginChanged = 1357003;
+
+    public static int kError_Async_CSRFCheckFailed = 1357004;
+
+    public static int Error_Global_NoError = 0;
+
+    public static int Error_Async_HttpConnectionFailed = 1001;
+
+    public static int Error_Async_UnexpectedNullResponse = 1002;
+
+    public static int Error_System_UIDNotFound = 1003;
+
+    public static int Error_System_ChannelNotFound = 1004;
+
+    public static int Error_System_PostFormIDNotFound = 1005;
+
+    public static int Error_Global_PostMethodError = 1006;
+
+    public static int Error_Global_GetMethodError = 1007;
+
+    public static int Error_Global_JSONError = 1008;
+
+    // 1357005: Error(1357005): Bad Parameter; There was an error understanding
+    // the request.
 }
